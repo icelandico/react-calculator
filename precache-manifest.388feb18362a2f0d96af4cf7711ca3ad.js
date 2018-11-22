3f2a38f2ae222ccb2ccfc79d7dc0d0900f595d1b
Michal Muszynski <m.muszy@gmail.com>
2018-11-22 20:18:25 +0100

    Updates

---
+++ precache-manifest.388feb18362a2f0d96af4cf7711ca3ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "63a079c4634bc1ecad6a",
+    "url": "/react-calculator/static/js/main.63a079c4.chunk.js"
+  },
+  {
+    "revision": "9693ae1b9745322b7c5e",
+    "url": "/react-calculator/static/js/1.9693ae1b.chunk.js"
+  },
+  {
+    "revision": "63a079c4634bc1ecad6a",
+    "url": "/react-calculator/static/css/main.e33d27e1.chunk.css"
+  },
+  {
+    "revision": "f86430d7791e9f4e8d5c043a5b77ed3d",
+    "url": "/react-calculator/index.html"
+  }
+];
